a59b8a45ff77e08c4ee9210730f2ad02f1034de7
Dong Seok Lee <dongseok.lee.log@gmail.com>
2025-01-29 11:19:26 +0900

    [test] add submodule

--- membership-service/src/main/java/com/fastcampuspay/TestApplication.java
+++ membership-service/src/main/java/com/fastcampuspay/TestApplication.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.fastcampuspay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- membership-service/src/main/java/com/fastcampuspay/TestController.java
+++ membership-service/src/main/java/com/fastcampuspay/TestController.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.fastcampuspay;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
